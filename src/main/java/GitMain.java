c90b7a4e32a81be16ad9408996df1160265d4ab7
sqq <335880822@qq.com>
2021-10-14 18:46:24 +0800

    secend commit

--- src/main/java/GitMain.java
+++ src/main/java/GitMain.java
@@ -7,5 +7,6 @@
 public class GitMain {
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
+        System.out.println("22222222222222222222");
     }
 }
