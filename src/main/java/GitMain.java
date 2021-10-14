8aecb9d3a72a1bac79247b5730a85ab72aacb63c
sqq <335880822@qq.com>
2021-10-14 17:30:52 +0800

    first commit

---
+++ src/main/java/GitMain.java
@@ -0,0 +1,11 @@
+/**
+ * @author: QinQunShi
+ * @Description:
+ * @Modified By:
+ * @create: create in 17:19 10 14 2021
+ */
+public class GitMain {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111111");
+    }
+}
