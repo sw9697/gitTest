c65ac09242ad216538bdfc1d8e7b840649ab005d
sqq <335880822@qq.com>
2022-07-05 18:05:36 +0800

    hot fix commit2

--- src/main/java/GitMain.java
+++ src/main/java/GitMain.java
@@ -14,5 +14,6 @@ public class GitMain {
         System.out.println("66666666666666666666");
         System.out.println("77777777777777777777");
         System.out.println("88888888888888888888");
+        System.out.println("hot_fix test");
     }
 }
